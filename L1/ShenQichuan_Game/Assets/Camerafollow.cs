34016ac10b4b60b154168ebcfbceb09437501d7d
RogerShen <shenqichuan@foxmail.com>
2020-10-02 00:09:23 +0800

    Version 1.0

---
+++ L1/ShenQichuan_Game/Assets/Camerafollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public GameObject player;  //主角
+    public float smoothing = 5f;
+    Vector3 offset;
+    public float minPosx;  //相机不超过背景边界允许的最小值
+    public float maxPosx;  //相机不超过背景边界允许的最大值
+    public float minPosy;  //相机不超过背景边界允许的最小值
+    public float maxPosy;  //相机不超过背景边界允许的最大值
+
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        FixCameraPos();
+    }
+
+    void FixCameraPos()
+    {
+        float pPosX = player.transform.position.x;  //主角 x轴方向时实坐标值
+        float cPosX = transform.position.x;             //相机 x轴方向时实坐标值
+        float pPosY = player.transform.position.y;
+        float cPosY = transform.position.y;
+
+        if ((pPosX - cPosX > 3 || pPosX - cPosX < -3 || pPosY - cPosY > 1 || pPosY - cPosY < -1))    // 并不是死死地跟随，是相机和主角之间距离超过某个值时才跟随
+        {
+            Vector3 playercampos = player.transform.position + offset;
+            if (playercampos.x > maxPosx) playercampos.x = maxPosx;
+            if (playercampos.x < minPosx) playercampos.x = minPosx;
+            // float realPosX = Mathf.Clamp(transform.position.x, minPosx, maxPosx);
+            // float realPosY = Mathf.Clamp(transform.position.y, minPosy, maxPosy);
+            // playercampos.x = realPosX;
+            // playercampos.y = realPosY;
+            transform.position = Vector3.Lerp(transform.position, playercampos, smoothing * Time.deltaTime);
+        }
+    }
+}
---
+++ L1/ShenQichuan_Game/Assets/Scripts/TilemapAPITest.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapAPITest : MonoBehaviour
+{
+
+    public Tilemap tilemap;
+    public TileBase tile;
+
+    public GameObject explosionPrefab;
+    private Grid grid;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    grid = GetComponent<Grid>();
+	    return;
+        //清空所有的格子
+	    //tilemap.ClearAllTiles();
+
+        //根据格子坐标获取瓦片
+	    TileBase tile1 = tilemap.GetTile(new Vector3Int(-4,-2,0));
+        Debug.Log(tile1);
+
+        //根据格子坐标设置瓦片
+	    tilemap.SetTile(Vector3Int.zero, tile);
+
+	    //删除指定格子的瓦片
+        tilemap.SetTile(Vector3Int.zero,null);
+
+        //将所有的参数1 ，替换成参数2
+        tilemap.SwapTile(tile1, tile);
+
+        //tilemap.SetTiles();
+        //tilemap.BoxFill();
+        //tilemap.GetTilesBlock()
+        //tilemap.SetTilesBlock();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+            //屏幕坐标转世界坐标
+	        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            //世界坐标转格子坐标
+	        Vector3Int cellPos = grid.WorldToCell(worldPos);
+
+	        ExplosionLogic(cellPos);
+	        ExplosionLogic(cellPos+new Vector3Int(1,0,0));
+	        ExplosionLogic(cellPos + new Vector3Int(-1, 0, 0));
+	        ExplosionLogic(cellPos + new Vector3Int(0, 1, 0));
+	        ExplosionLogic(cellPos + new Vector3Int(0, -1, 0));
+        }
+	}
+
+    void ExplosionLogic(Vector3Int cellPos)
+    {
+        tilemap.SetTile(cellPos, null);
+
+        //格子坐标转世界坐标
+        //Vector3 worldPos = grid.CellToWorld(cellPos);
+        Vector3 worldPos = grid.CellToLocalInterpolated(cellPos + new Vector3(0.5f, 0.5f, 0f));
+        Instantiate(explosionPrefab, worldPos, Quaternion.identity);
+    }
+}
